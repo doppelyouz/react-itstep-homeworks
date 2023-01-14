38d1ba138fc85218fb7a28469f7936c8a5be3fb4
anikisana <younker12345@gmail.com>
2023-01-15 01:33:18 +0600

    fetch users, useeffect

---
+++ homework/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles/index.css';
+
+import HomeworkUsers from "./pages/homeworkUseEffectFetchUsers";
+
+function App() {
+  return (
+    <div className="App">
+      <HomeworkUsers />
+    </div>
+  );
+}
+
+export default App;
---
+++ homework/src/pages/homeworkUseEffectFetchUsers/HomeworkUsers.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomeworkUsers.scss';
+
+
+import {Stand} from '../../components/homeworkUseEffect'
+
+const HomeworkUsers = () => {
+    return (
+        <Stand />
+    )
+}
+
+export default HomeworkUsers;
